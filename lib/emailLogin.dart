ce6640ebabf55e7f08a02fc8879ce13df7d40e0a
richard0220 <richard0311312@gmail.com>
2021-07-30 13:11:25 +0800

    Adding email login function

---
+++ lib/emailLogin.dart
@@ -0,0 +1,51 @@
+import 'package:email_auth/email_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class emailServer {
+  String signUpHint = '';
+
+  // register with email
+  Future<void> emailSignUp(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        signUpHint = 'The password is too weak';
+      } else if (e.code == 'email-already-in-use') {
+        signUpHint = 'The email account is already exist';
+      }
+    }
+  }
+
+  // Send OPT to verify email
+  void sendOTP(String email) async {
+    EmailAuth.sessionName = 'Study-Helper';
+    var res = await EmailAuth.sendOtp(receiverMail: email);
+    if (res) {
+      print('Send successfully!');
+    } else {
+      print('Failed to send OTP');
+    }
+  }
+
+  // Check the OTP code
+  void verifyOTP(String email, String userOTP) {
+    var res = EmailAuth.validate(receiverMail: email, userOTP: userOTP);
+    if (res) {
+      print('Verified!');
+    } else {
+      print('Wrong OTP');
+    }
+  }
+
+  // Sign in with email
+  Future<void> emailSignIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+    }
+  }
+}
